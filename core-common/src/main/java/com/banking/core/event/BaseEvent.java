5d2d2918e12c5a7a878f1dc99551dea8a630059e
Pedro Candido <pedrocsalvio@gmail.com>
2025-11-22 15:52:48 -0300

    feat: abstract class of an Event

---
+++ core-common/src/main/java/com/banking/core/event/BaseEvent.java
@@ -0,0 +1,18 @@
+package com.banking.core.event;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.experimental.SuperBuilder;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@SuperBuilder
+public abstract class BaseEvent {
+    private UUID id;
+    private LocalDateTime eventDateTime;
+}
