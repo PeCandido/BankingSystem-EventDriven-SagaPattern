82f25e6be100177e49371cf9a00aa617d156bafe
Pedro Candido <pedrocsalvio@gmail.com>
2025-11-23 18:18:58 -0300

    feat: implementing Payment Entity

---
+++ payment-service/src/main/java/com/banking/payment/model/PaymentEntity.java
@@ -0,0 +1,31 @@
+package com.banking.payment.model;
+
+import com.banking.core.enums.PaymentStatus;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+import java.util.UUID;
+
+@Entity
+@Table(name = "payments")
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class PaymentEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private UUID id;
+    private UUID payerId;
+    private UUID payeeId;
+    private BigDecimal amount;
+    private String currency;
+
+    @Enumerated(EnumType.STRING)
+    private PaymentStatus status;
+}
