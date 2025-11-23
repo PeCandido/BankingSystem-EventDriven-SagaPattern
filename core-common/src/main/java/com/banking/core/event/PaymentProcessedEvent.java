81319a1cac69c0d43a5d1e26093b00177c6da6d0
Pedro Candido <pedrocsalvio@gmail.com>
2025-11-23 18:16:42 -0300

    chore: adding spring boot notations

--- core-common/src/main/java/com/banking/core/event/PaymentProcessedEvent.java
+++ core-common/src/main/java/com/banking/core/event/PaymentProcessedEvent.java
@@ -1,9 +1,19 @@
 package com.banking.core.event;
 
 import com.banking.core.enums.PaymentStatus;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+import lombok.experimental.SuperBuilder;
 
 import java.util.UUID;
 
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@SuperBuilder
+@EqualsAndHashCode(callSuper = false)
 public class PaymentProcessedEvent extends BaseEvent{
     private UUID paymentId;
     private UUID payerId;
