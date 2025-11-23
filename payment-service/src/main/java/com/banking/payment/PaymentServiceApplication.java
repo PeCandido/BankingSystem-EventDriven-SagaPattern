f33a788370f3af8c97b7e664b107c8b37f56df23
Pedro Candido <pedrocsalvio@gmail.com>
2025-11-23 18:18:07 -0300

    chore: moved to package com.banking.payment

--- payment-service/src/main/java/com/banking/payment/PaymentServiceApplication.java
+++ payment-service/src/main/java/com/banking/payment/PaymentServiceApplication.java
@@ -1,4 +1,4 @@
-package com.banking.paymentservice;
+package com.banking.payment;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
