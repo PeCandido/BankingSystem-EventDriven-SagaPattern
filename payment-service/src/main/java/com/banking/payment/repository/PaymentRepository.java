3c93811573a483bad84545084f36abc14b84f28e
Pedro Candido <pedrocsalvio@gmail.com>
2025-11-23 18:21:35 -0300

    feat: implementing Payment JPARepository

---
+++ payment-service/src/main/java/com/banking/payment/repository/PaymentRepository.java
@@ -0,0 +1,12 @@
+package com.banking.payment.repository;
+
+import com.banking.payment.model.PaymentEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface PaymentRepository extends JpaRepository<PaymentEntity, UUID> {
+
+}
