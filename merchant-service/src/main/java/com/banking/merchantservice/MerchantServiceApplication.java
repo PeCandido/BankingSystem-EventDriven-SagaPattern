9718213fdbbc92c99cf887ae57a8024847063089
Alan-VSouza <alansouzeesi@gmail.com>
2025-11-26 22:47:25 -0300

    feat(merchant): add MerchantServiceApplication bootstrap class

--- merchant-service/src/main/java/com/banking/merchantservice/MerchantServiceApplication.java
+++ merchant-service/src/main/java/com/banking/merchantservice/MerchantServiceApplication.java
@@ -2,12 +2,13 @@ package com.banking.merchantservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = {"com.banking.merchantservice", "com.banking.core"})
 public class MerchantServiceApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(MerchantServiceApplication.class, args);
     }
-
 }
