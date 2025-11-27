6132429b3a3479f98c06dedc82a6c996984eed06
Alan-VSouza <alansouzeesi@gmail.com>
2025-11-26 22:47:39 -0300

    fix(payment): remove @GeneratedValue to fix UUID generation conflict

--- payment-service/src/main/java/com/banking/payment/model/PaymentEntity.java
+++ payment-service/src/main/java/com/banking/payment/model/PaymentEntity.java
@@ -19,7 +19,6 @@ import java.util.UUID;
 public class PaymentEntity {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
     private UUID id;
     private UUID payerId;
     private UUID payeeId;
