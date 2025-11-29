e66400b9cdc58630bd818cb37e2f5a7a4ed356f8
Pedro Candido <pedrocsalvio@gmail.com>
2025-11-29 15:54:28 -0300

    feat: implementing GET methods in Payment Controller

--- payment-service/src/main/java/com/banking/payment/controller/PaymentController.java
+++ payment-service/src/main/java/com/banking/payment/controller/PaymentController.java
@@ -1,12 +1,12 @@
 package com.banking.payment.controller;
 
+import com.banking.payment.controller.dto.PaymentDetailsDto;
 import com.banking.payment.controller.dto.PaymentDto;
 import com.banking.payment.controller.dto.PaymentResponseDto;
 import com.banking.payment.model.PaymentEventEntity;
 import com.banking.payment.service.PaymentService;
 import com.banking.payment.service.PaymentEventStore;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -26,6 +26,20 @@ public class PaymentController {
         return ResponseEntity.accepted().body(
                 new PaymentResponseDto(paymentId, "Payment being processed")
         );
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<PaymentDetailsDto> getPayment(@PathVariable UUID id) {
+        return ResponseEntity.ok(paymentService.getPaymentById(id));
+    }
+
+    @GetMapping
+    public ResponseEntity<List<PaymentDetailsDto>> getPayments(@RequestParam(required = false) UUID payerId) {
+        if(payerId != null) {
+            return ResponseEntity.ok(paymentService.getPaymentsByPayer(payerId));
+        }
+
+        return ResponseEntity.ok(List.of());
     }
 
     @GetMapping("/{paymentId}/events")
--- payment-service/src/main/java/com/banking/payment/controller/dto/PaymentResponseDto.java
+++ payment-service/src/main/java/com/banking/payment/controller/dto/PaymentResponseDto.java
@@ -2,4 +2,7 @@ package com.banking.payment.controller.dto;
 
 import java.util.UUID;
 
-public record PaymentResponseDto(UUID paymentId, String message) {}
+public record PaymentResponseDto(
+        UUID paymentId,
+        String message
+) {}
