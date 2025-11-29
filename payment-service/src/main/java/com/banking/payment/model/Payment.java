4922956a9f1ef782dc454c091d8d905b121e0939
Pedro Candido <pedrocsalvio@gmail.com>
2025-11-29 15:53:22 -0300

    feat: implementing validation logic, approve and reject methods

--- payment-service/src/main/java/com/banking/payment/model/Payment.java
+++ payment-service/src/main/java/com/banking/payment/model/Payment.java
@@ -1,6 +1,7 @@
 package com.banking.payment.model;
 
 import com.banking.core.enums.PaymentStatus;
+import com.banking.payment.exception.InvalidPaymentException;
 import lombok.*;
 
 import java.math.BigDecimal;
@@ -18,7 +19,41 @@ public class Payment {
     private String currency;
     private PaymentStatus status;
 
+    public void validate(){
+        if(amount == null || amount.compareTo(BigDecimal.ZERO) <= 0){
+            throw new InvalidPaymentException("Amount must be greater than zero");
+        }
+
+        if(currency == null || currency.trim().isEmpty()){
+            throw new InvalidPaymentException("Currency cannot be null or empty");
+        }
+
+        if(payerId == null){
+            throw new InvalidPaymentException("Payer ID cannot be null or empty");
+        }
+
+        if(payeeId == null){
+            throw new InvalidPaymentException("Payee id is null");
+        }
+    }
+
     public void initialize(){
+        validate();
         this.status = PaymentStatus.PENDING;
+    }
+
+    public void approve(){
+        if(this.status != PaymentStatus.PENDING){
+            throw new InvalidPaymentException("Only PENDING payment can be approved");
+        }
+
+        this.status = PaymentStatus.APPROVED;
+    }
+
+    public void reject(){
+        if(this.status != PaymentStatus.PENDING){
+            throw new InvalidPaymentException("Only PENDING payment can be rejected");
+        }
+        this.status = PaymentStatus.REJECTED;
     }
 }
