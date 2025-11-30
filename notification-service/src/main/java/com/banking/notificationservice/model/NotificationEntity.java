36b7f572fdd93b0128cbf3dbb60825fffd3c0ecb
Turazzi <analivia.turazzi@gmail.com>
2025-11-30 01:08:05 -0300

    feat: Adds NotificationEntity class

---
+++ notification-service/src/main/java/com/banking/notificationservice/model/NotificationEntity.java
@@ -0,0 +1,36 @@
+package com.banking.notificationservice.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Entity
+@Table(name = "notifications")
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class NotificationEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @Column(nullable = false)
+    private UUID paymentId;
+    private String recipientEmail;
+    private String subject;
+
+    @Column(columnDefinition = "TEXT")
+    private String content;
+    private LocalDateTime sentAt;
+
+
+
+}
