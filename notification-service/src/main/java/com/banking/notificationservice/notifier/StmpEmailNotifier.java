333508a3b124c1242bb3329bf8c7d98ddf9c3db0
Turazzi <analivia.turazzi@gmail.com>
2025-12-01 00:35:07 -0300

    feat: create a class to send real emails.

--- notification-service/src/main/java/com/banking/notificationservice/notifier/StmpEmailNotifier.java
+++ notification-service/src/main/java/com/banking/notificationservice/notifier/StmpEmailNotifier.java
@@ -1,4 +1,40 @@
 package com.banking.notificationservice.notifier;
 
-public class StmpEmailNotifier {
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Primary;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Component;
+
+@Slf4j
+@Component("realNotifier")
+@Primary
+@RequiredArgsConstructor
+public class StmpEmailNotifier implements EmailNotifier {
+
+    private final JavaMailSender mailSender;
+
+    @Override
+    public void send(String to, String subject, String body) {
+
+        try {
+            log.info("Trying to send real email by SMTP to: {}", to);
+
+            SimpleMailMessage message = new SimpleMailMessage();
+            message.setFrom("noreply@banking-system.com");
+            message.setTo(to);
+            message.setSubject(subject);
+            message.setText(body);
+
+            mailSender.send(message);
+
+            log.info("Email sent");
+        } catch (Exception e) {
+            log.error("Error sending email", e);
+
+        }
+
+    }
+
 }
